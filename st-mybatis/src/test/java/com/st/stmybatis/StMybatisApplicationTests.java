fdec0a54b981a527be966dcc512df039fa1ae0ab
squirrelzt <squirrelzt@126.com>
2020-06-04 10:38:40 +0800

    update

---
+++ st-mybatis/src/test/java/com/st/stmybatis/StMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.st.stmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
