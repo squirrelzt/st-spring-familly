f1986d302b27523b81826e5805813e78700dd648
squirrelzt <squirrelzt@126.com>
2020-06-05 20:47:57 +0800

    代码生成model、mapper、xml文件

---
+++ st-mybatis-generate/src/main/java/com/stmybatisgenerate/StMybatisGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.stmybatisgenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StMybatisGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StMybatisGenerateApplication.class, args);
+	}
+
+}
---
+++ st-mybatis-generate/src/test/java/com/stmybatisgenerate/StMybatisGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stmybatisgenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMybatisGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
