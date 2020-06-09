09a71f1a920d6ef209a4d25b96ebc4e2e4dd2581
squirrelzt <squirrelzt@126.com>
2020-06-09 17:22:16 +0800

    mybatis打印sql日志

--- st-mybatis-enum/src/main/java/com/stmybatisenum/enums/ZipCodeEnum.java
+++ st-mybatis-enum/src/main/java/com/stmybatisenum/enums/ZipCodeEnum.java
@@ -1,5 +1,16 @@
 package com.stmybatisenum.enums;
 
 public enum ZipCodeEnum {
+    BEIJING(100),
+    SHENZHEN(200),
+    HEBEI(300);
 
+    private Integer code;
+    ZipCodeEnum(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
 }
