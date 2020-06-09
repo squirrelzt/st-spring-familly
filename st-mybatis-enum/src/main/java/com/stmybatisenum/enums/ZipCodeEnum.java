b439916a6c142ed707e103af2e76e713029cb809
squirrelzt <squirrelzt@126.com>
2020-06-09 16:24:45 +0800

    mybatis enum

---
+++ st-mybatis-enum/src/main/java/com/stmybatisenum/enums/ZipCodeEnum.java
@@ -0,0 +1,5 @@
+package com.stmybatisenum.enums;
+
+public enum ZipCodeEnum {
+
+}
---
+++ st-mybatis-enum/src/test/java/com/stmybatisenum/StMybatisEnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stmybatisenum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMybatisEnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
