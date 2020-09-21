c81bd83ab7e86cb8e2ca8190e0aca806d81fa1a6
squirrelzt <squirrelzt@126.com>
2020-09-21 16:22:31 +0800

    dubbo springboot rest服务Get请求@PathParam和@QueryParam

--- st-dubbo-springboot/dubbo-provider/src/main/java/com/dubbo/SayHelloRestServiceImpl.java
+++ st-dubbo-springboot/dubbo-provider/src/main/java/com/dubbo/SayHelloRestServiceImpl.java
@@ -2,20 +2,23 @@ package com.dubbo;
 
 import com.dubbo.domain.Person;
 import com.dubbo.domain.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
+@Slf4j
 @DubboService(protocol = {"rest"})
 public class SayHelloRestServiceImpl implements ISayHelloRestService {
 
     @Override
-    public Response sayHelloRest() {
+    public Response sayHelloRest(String param1, String param2) {
+        log.info("param1={},param2={}", param1, param2);
         Person person = new Person();
         person.setName("tom");
         person.setAge(30);
         person.setGender("man");
         Response response = new Response();
         response.setCode("200");
-        response.setMsg("成功");
+        response.setMsg(param1 + " " + param2);
         response.setData(person);
         return response;
     }
--- st-dubbo-springboot/server-api/src/main/java/com/dubbo/ISayHelloRestService.java
+++ st-dubbo-springboot/server-api/src/main/java/com/dubbo/ISayHelloRestService.java
@@ -8,9 +8,9 @@ import javax.ws.rs.core.MediaType;
 @Path("/say")
 public interface ISayHelloRestService {
 
-    @POST
-    @Path("/helloRest")
+    @GET
+    @Path("/helloRest/{param1}")
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes({MediaType.APPLICATION_JSON})
-    Response sayHelloRest();
+    Response sayHelloRest(@PathParam("param1") String param1, @QueryParam("param2") String param2);
 }
